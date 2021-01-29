e04495c784ca34efad003fda475af93dfed83614
madeblanka <38938512+madeblanka@users.noreply.github.com>
2021-01-29 17:01:12 +0800

    crud aja

---
+++ app/Http/Controllers/BiodataController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Biodata;
+use Illuminate\Http\Request;
+
+class BiodataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $biodata = Biodata::latest()->paginate(5);
+        return view('biodata.index',compact('biodata'))->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('biodata.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'alamat' => 'required',
+        ]);
+        Biodata::create($request->all());
+        return redirect()->route('biodata.index')->with('success','Data berhasil di input');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Biodata  $biodata
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Biodata $biodata)
+    {
+        return view('biodata.show',compact('biodata'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Biodata  $biodata
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Biodata $biodata)
+    {
+        return view('biodata.edit',compact('biodata'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Biodata  $biodata
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Biodata $biodata)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'alamat' => 'required',
+        ]);
+        $biodata->update($request->all());
+        return redirect()->route('biodata.index')->with('success','biodata berhasil di update');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Biodata  $biodata
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Biodata $biodata)
+    {
+        $biodata->delete();
+        return redirect()->route('biodata.index')->with('success','Biodata berhasil dihapus');
+    }
+}
